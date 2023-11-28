81c62dbbf3ee0e7d9b20755b09317074e012dd4b
EngRibeiro <85964596+EngRibeiro@users.noreply.github.com>
2023-11-28 11:29:39 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/global.css">
+    <title>Eng° Ribeiro</title>
+</head>
+<body>
+
+    <header>
+        <section>
+                <div>CAUTION</div>
+        </section>
+        <section>
+                <div>
+                    O CONTEUDO PRESENTE NESTE SITE É REFERENCIAL 
+                    À AS HABILIDADES, CONHECIMENTOS E EXPERIÊNCIA
+                    DE VIDA DO PROPRIETÁRIO, COMO FORMA DE PRESERVAR
+                    PARTE DA MEMÓRIA DO ENGENHEIRO.  
+                </div>
+        </section>
+    </header>    
+    
+    <main>
+
+    </main>
+</body>
+</html>
